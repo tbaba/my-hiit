8423202645fa5f353336f9739bc37573aabaf1d7
Tatsuro Baba <harakirisoul@gmail.com>
2025-09-10 13:32:14 +0900

    feat: display tagline

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>my-hiit</Text>
+      <Text style={styles.tagline}>HIITワークアウトを便利に行うアプリ</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  tagline: {
+    marginTop: 8,
+    fontSize: 16,
+  },
+});
